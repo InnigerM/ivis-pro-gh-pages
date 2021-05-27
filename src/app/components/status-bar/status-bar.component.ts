3f3996f671e6adbfb55efcff8db6901989652165
InnigerM <marco.inniger@gmail.com>
2021-05-27 15:36:49 +0200

    Fixed edit mode on stepper

--- src/app/components/status-bar/status-bar.component.ts
+++ src/app/components/status-bar/status-bar.component.ts
@@ -8,16 +8,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class StatusBarComponent implements OnInit {
   isLinear = false;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
+  emptyFormGroup: FormGroup;
+  leagueSelectionForm: FormGroup;
 
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.firstFormGroup = this._formBuilder.group({
+    this.emptyFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
-    this.secondFormGroup = this._formBuilder.group({
+    this.leagueSelectionForm = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
   }
